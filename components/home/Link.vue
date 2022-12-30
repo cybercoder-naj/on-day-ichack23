dfb97c9ca48a08c714bdf07213a6365e3c297307
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2022-12-30 10:38:45 +0530

    added home page

---
+++ components/home/Link.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+interface HomeLink {
+    title: string
+}
+
+const props = defineProps<HomeLink>()
+</script>
+
+<template>
+    <div class="home-link">
+        <h1>{{ title }}</h1>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt qui sint, vero magni possimus illum. Dolores
+        soluta aperiam numquam iste delectus consequuntur eveniet fugit id asperiores magnam? Distinctio, itaque odio?</p>
+    </div>
+</template>
+
+<style scoped>
+.home-link {
+    background-color: blue;
+    width: 30%;
+    border-radius: 2em;
+    padding: 16px 20px;
+}
+
+.home-link h1 {
+    color: cyan;
+    font-family: monospace;
+    margin-bottom: 0.5em;
+}
+
+.home-link p {
+    color: white;
+}
+</style>
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -4,7 +4,7 @@ const router = useRouter()
 
 onMounted(() => {
     if (!user || !user.value) {
-        router.push("/login")
+        // router.push("/login")
     }
 })
 
@@ -17,7 +17,79 @@ async function signOut() {
 </script>
 
 <template>
-    {{ user }}
-    <br><br>
-    <button @click="signOut">Log out</button>
+    <div id="home">
+        <div class="header">
+            <div>My Ticket</div>
+        </div>
+        <div class="message">
+            <h1>Message From Organizers</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima hic, corrupti sapiente eos tempore nobis
+                dolore voluptate ab sunt maiores ipsa, expedita sed quis voluptas reprehenderit rerum magni dignissimos?
+                Amet? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error illum sequi quia eligendi.
+                Reiciendis odit corporis facere illum, praesentium ducimus mollitia voluptates laborum! In cum quisquam
+                ipsam libero asperiores vel!</p>
+            <p class="time-updated">Last updated 3 minutes ago</p>
+        </div>
+        <div class="links">
+            <div class="row">
+                <HomeLink title="Get started" />
+                <HomeLink title="Hack Hub" />
+                <HomeLink title="HelpQ" />
+            </div>
+            <div class="row">
+                <HomeLink title="Submission" />
+                <HomeLink title="Code of Conduct" />
+                <HomeLink title="Schedule" />
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+#home {
+    background-color: hotpink;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0 20vw;
+}
+
+.header {
+    align-self: flex-end;
+    margin-right: 2em;
+    margin-bottom: 2em;
+    padding: 12px 20px;
+    background-color: blue;
+    color: cyan;
+    border-radius: 2em;
+    font-size: 1.2em;
+    font-weight: 700;
+    font-style: italic;
+}
+
+.message {
+    background-color: pink;
+    padding: 1.5em;
+    border-radius: 2em;
+}
+
+.message h1 {
+    margin-bottom: 2em;
+    color: blue;
+    font-family: monospace;
+}
+
+.links {
+    padding: 0 2em;
+    margin-top: 1em;
+}
+
+.links .row {
+    display: flex;
+    justify-content: space-between;
+    margin: 1em 0;
+}
+</style>
